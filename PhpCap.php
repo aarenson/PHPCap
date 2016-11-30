ff7e998a741ff4c244c358b3287f83c10c6e0c66
Andy Arenson <aarenson@iu.edu>
2016-11-30 11:23:25 -0500

    Fold in new functions from OPT2ETL

--- PhpCap.php
+++ PhpCap.php
@@ -36,7 +36,17 @@ class REDCapProject {
   protected $token = '';     // API token for a REDCap project
   protected $notifier = '';  // Object for reporting errors
 
-  protected $primary = '';   // The field that stores the record_id
+  protected $metadata = array();
+  protected $fieldnames = array();
+  protected $exportfieldnames = array();
+
+  // Used for storing info about batches between calls to
+  // get_records_by_ids_batch
+  protected $records_by_ids_batch_error = '';
+  protected $records_by_ids_batch_size = 1;
+  protected $records_by_ids_batch_record_ids = array();
+
+  public $primary = '';   // The field that stores the record_id
 
   public function __construct($app,$api_url,$token,$notifier,$primary) {
     $this->app = $app;
@@ -155,6 +165,65 @@ class REDCapProject {
 
 
   //-----------------------------------------------------------------------
+  // prep_records_by_ids_batch
+  //
+  // $error is used if export fails
+  // 
+  public function prep_records_by_ids_batch($error,$size,$record_ids) {
+
+    $this->records_by_ids_batch_error = $error;
+    $this->records_by_ids_batch_size = $size;
+    $this->records_by_ids_batch_record_ids = $record_ids;
+    
+    return(true);
+  }
+  // END prep_records_by_ids_batch
+  //-------------------------------------------------------------------------
+
+
+  //-----------------------------------------------------------------------
+  // get_records_by_ids_batch
+  //
+  // $error is used if export fails
+  //
+  // $batch => array($record_id => $records);
+  // There can be more than one record for longitudinal projects
+  // 
+  public function get_records_by_ids_batch() {
+
+    $batch = array();
+    $cur_records = array();
+
+    $cur_records = array_splice($this->records_by_ids_batch_record_ids,
+				0,
+			        $this->records_by_ids_batch_size);
+
+    if (!empty($cur_records)) {
+
+      $cur_error = $this->records_by_ids_batch_error.
+    	           " - starting with record: '".$cur_records[0]."";
+      $results = $this->get_records_by_ids($cur_error,$cur_records);
+
+      // Set up $batch results
+      foreach($results as $result) {
+        $primary_id = $result[$this->primary];
+
+        // If no results yet for this record, create array
+        if (!array_key_exists($primary_id,$batch)) {
+      	  $batch[$primary_id] = array();
+        }
+
+        array_push($batch[$primary_id],$result);
+      }
+    }
+
+    return($batch);
+  }
+  // END get_records_by_ids_batch
+  //-------------------------------------------------------------------------
+
+
+  //-----------------------------------------------------------------------
   // get_records_by_fields
   //
   // $error is used if export fails
@@ -238,6 +307,29 @@ class REDCapProject {
 
 
   //-------------------------------------------------------------------------
+  // get_file_from_project
+  //
+  // $error is used if export fails
+  // 
+  public function get_file_from_project($record_id,$field,$error) {
+
+    $data = array(
+    	          'content' => 'file',
+                  'action' => 'export',
+                  'record' => $record_id,
+                  'field' => $field,
+                  'returnFormat' => 'json'
+                  );
+
+    $results = $this->export($error,$data);
+
+    return($results);
+  }
+  // END get_file_from_project
+  //-------------------------------------------------------------------------
+
+
+  //-------------------------------------------------------------------------
   // export
   //
   // $error is used if export fails
@@ -309,6 +401,332 @@ class REDCapProject {
   }
 
   // END export
+  //-------------------------------------------------------------------------
+
+
+  //-----------------------------------------------------------------------
+  // get_lookup_choices
+  //
+  // $error is used if export_metadata fails
+  //
+  // $results = array($field_name1 => array($category1 => $label1, ...),
+  //                  ...)     	      			  
+  // 
+  public function get_lookup_choices() {
+
+    $results = array();
+
+    // Get all metadata
+    $error = 'Unable to retrieve metadata while getting lookup choices';
+    $fields = $this->get_metadata($error,'','');
+     
+    // Foreach field
+    foreach ($fields as $field) {
+
+      // Check the type of field
+      switch ($field['field_type']) {
+
+        // If it's a radio, dropdown, or checkbox field
+        case 'radio':		  
+	case 'dropdown':      
+	case 'checkbox':      
+	  
+	  // Get the choices
+	  $choices_str = $field['select_choices_or_calculations'];
+	  $choices = array_map('trim', explode("|", $choices_str));
+
+	  $field_results = array();
+
+	  // Foreach choice
+	  foreach ($choices as $choice) {
+
+	    if ($choice === "") {
+	       continue;
+	    }
+
+	    // Get the category and label
+	    list ($category, $label) = 
+	      array_map('trim', explode(",",$choice,2));
+
+	    // Add them to the results for this field
+	    $field_results[$category] = $label;
+
+	  }
+
+	  // Add this field to the overall results
+	  $results[$field['field_name']] = $field_results;
+
+          break;
+
+        default:
+          break;
+
+      }  // end switch
+    }  // end foreach
+
+    return $results;
+  }
+
+  // END export
+  //-------------------------------------------------------------------------
+
+
+  //-----------------------------------------------------------------------
+  // get_metadata
+  //
+  // $error is used if export_metadata fails
+  // 
+  public function get_metadata($error,$fields,$forms) {
+
+    if (empty($this->metadata)) {
+
+       $data = array('content' => 'metadata', 
+       		     'type' => 'flat', 
+		     'format' => 'json', 
+		     'fields' => $fields, 
+		     'forms' => $forms, 
+		     );  
+
+       $new_message = $this->export_metadata($error,$data);
+       $this->metadata = $new_message[0];
+     }
+
+    return($this->metadata);
+  }
+  // END get_metadata
+  //-------------------------------------------------------------------------
+
+
+  //-------------------------------------------------------------------------
+  // export_medata
+  //
+  // $error is used if export fails
+  // 
+  // $data is array to use in Rest Request
+  //
+  public function export_metadata($error,$data) {
+
+    $data['token'] = $this->token;
+
+    // Create REDCap API request object
+    $request = new RestCallRequest($this->api_url, 'POST', $data);
+
+    // Initiate the API request, and fetch the results from the request object.
+    $request->execute();
+    $body_str = $request->getResponseBody();
+    $header_array = $request->getResponseInfo();
+
+    // Decode the JSON content returned by REDCap (into an array rather
+    // than into an object by specifying the second argument as "true"),
+    if (empty($body_str)) {
+      $body_array = array();
+    }
+    else {
+      $body_array = json_decode($body_str, true);
+    }
+
+    if( (! isset($body_array)) || 
+	(empty($body_array)) || 
+	(array_key_exists('error',$body_array)) ) {
+
+      if (array_key_exists('error',$body_array)) {
+	$error .= "Error: '".$body_array['error']."'\n";
+      }
+
+      $this->notifier->notify($error);
+
+      return false;
+    }
+
+    return(array($body_array,$header_array));
+  }
+
+  // END export_metadata
+  //-------------------------------------------------------------------------
+
+
+  //-----------------------------------------------------------------------
+  // get_fieldnames
+  //
+  // $error is used if export_exportfieldnames fails
+  //
+  // $results = array($field_name1 => 1, ...)
+  // 
+  public function get_fieldnames() {
+
+    if (empty($this->fieldnames)) {
+
+      // Get export_field names
+      $error = 'Unable to retrieve exportfieldnames while getting field names';
+      $fields = $this->get_exportfieldnames($error,'');
+     
+      // Foreach field
+      foreach ($fields as $field) {
+        $this->fieldnames[$field['export_field_name']] = 1;
+      }
+    }
+
+    return($this->fieldnames);
+  }
+
+  // END get_fieldnames
+  //-------------------------------------------------------------------------
+
+
+  //-----------------------------------------------------------------------
+  // get_exportfieldnames
+  //
+  // $error is used if export_exportfieldnames fails
+  // 
+  public function get_exportfieldnames($error,$field) {
+
+    if (empty($this->exportfieldnames)) {
+
+       $data = array('content' => 'exportFieldNames', 
+		     'format' => 'json', 
+		     'field' => $field, 
+		     );  
+
+       $new_message = $this->export_exportfieldnames($error,$data);
+       $this->exportfieldnames = $new_message[0];
+     }
+
+    return($this->exportfieldnames);
+  }
+  // END get_exportfieldnames
+  //-------------------------------------------------------------------------
+
+
+  //-------------------------------------------------------------------------
+  // export_exportfieldnames
+  //
+  // $error is used if export fails
+  // 
+  // $data is array to use in Rest Request
+  //
+  public function export_exportfieldnames($error,$data) {
+
+    $data['token'] = $this->token;
+
+    // Create REDCap API request object
+    $request = new RestCallRequest($this->api_url, 'POST', $data);
+
+    // Initiate the API request, and fetch the results from the request object.
+    $request->execute();
+    $body_str = $request->getResponseBody();
+    $header_array = $request->getResponseInfo();
+
+    // Decode the JSON content returned by REDCap (into an array rather
+    // than into an object by specifying the second argument as "true"),
+    if (empty($body_str)) {
+      $body_array = array();
+    }
+    else {
+      $body_array = json_decode($body_str, true);
+    }
+
+    if( (! isset($body_array)) || 
+	(empty($body_array)) || 
+	(array_key_exists('error',$body_array)) ) {
+
+      if (array_key_exists('error',$body_array)) {
+	$error .= "Error: '".$body_array['error']."'\n";
+      }
+
+      $this->notifier->notify($error);
+
+      return false;
+    }
+
+    return(array($body_array,$header_array));
+  }
+
+  // END export_exportfieldnames
+  //-------------------------------------------------------------------------
+
+
+  //-----------------------------------------------------------------------
+  // get_project_id
+  //
+  // $error is used if export_project_info fails
+  // 
+  public function get_project_id($error) {
+
+    $results = $this->get_project_info($error);
+
+    $project_id = $results['project_id'];
+
+    return($project_id);
+  }
+
+  // END get_project_id
+  //-------------------------------------------------------------------------
+
+
+  //-----------------------------------------------------------------------
+  // get_project_info
+  //
+  // $error is used if export_project_info fails
+  // 
+  public function get_project_info($error) {
+
+    $data = array('content' => 'project', 
+		  'format' => 'json', 
+		  );  
+
+    $new_message = $this->export_metadata($error,$data);
+
+    return($new_message[0]);
+  }
+  // END get_project_info
+  //-------------------------------------------------------------------------
+
+
+  //-------------------------------------------------------------------------
+  // export_project_info
+  //
+  // $error is used if export fails
+  // 
+  // $data is array to use in Rest Request
+  //
+  public function export_project_info($error,$data) {
+
+    $data['token'] = $this->token;
+
+    // Create REDCap API request object
+    $request = new RestCallRequest($this->api_url, 'POST', $data);
+
+    // Initiate the API request, and fetch the results from the request object.
+    $request->execute();
+    $body_str = $request->getResponseBody();
+    $header_array = $request->getResponseInfo();
+
+    // Decode the JSON content returned by REDCap (into an array rather
+    // than into an object by specifying the second argument as "true"),
+    if (empty($body_str)) {
+      $body_array = array();
+    }
+    else {
+      $body_array = json_decode($body_str, true);
+    }
+
+    if( (! isset($body_array)) || 
+	(empty($body_array)) || 
+	(array_key_exists('error',$body_array)) ) {
+
+      if (array_key_exists('error',$body_array)) {
+	$error .= "Error: '".$body_array['error']."'\n";
+      }
+
+      $this->notifier->notify($error);
+
+      return false;
+    }
+
+    return(array($body_array,$header_array));
+  }
+
+  // END export_project_info
   //-------------------------------------------------------------------------
 
 
@@ -584,7 +1002,7 @@ class REDCapDETHandler {
   //
   public function check_det_id($det_id) {
 
-    if ($det_id !== $this->pid) {
+    if ((int) $det_id !== (int) $this->pid) {
 
       $error = "Project id supplied by data entry trigger ('".$det_id."') ".
 	"does not match expected id for survey ('".$this->pid."').";
