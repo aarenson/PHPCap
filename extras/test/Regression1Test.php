3ffaff6f67f7ebcb63b750c6244280ae92dc23cd
Andy Arenson <aarenson@iu.edu>
2016-11-30 12:50:48 -0500

    Extract configuration for tests into separate class

--- extras/test/Regression1Test.php
+++ extras/test/Regression1Test.php
@@ -1,28 +1,11 @@
 <?php
 
-require_once('PHPUnit/Autoload.php');
+require_once('extras/test/PhpCap_TestCase.php');
 
-require_once('SampleNotifier.php');
-require_once('PhpCap.php');
-
-class Regression1Test extends PHPUnit_Framework_TestCase {
+class Regression1Test extends PhpCap_TestCase {
 
    public function testRegression1 () {
 
-     $test_name = 'Regression Test 1';
-
-     $from_email = 'aarenson@iu.edu';
-     $subject_token = '['.$test_name.'] Error';
-     $redcap_url = 'https://redcap.uits.iu.edu/api/';
-     $api_token = 'C5477C91801C64BB1C0BD65DEDB6C354';
-     $primary_key = 'record_id';
-
-     // Create factory
-     $app = $test_name;
-     $notifier = new SampleNotifier($from_email,$from_email,$subject_token);
-     $apifactory = new REDCapFactory($app,$redcap_url,$notifier);
-     $project = $apifactory->make_project($api_token,$primary_key);
-      
      // Get results
      $fields_filter = array(
 			    'dropdown_numeric' => '2',
@@ -31,7 +14,7 @@ class Regression1Test extends PHPUnit_Framework_TestCase {
 			    );
 			    
      $results = 
-       $project->get_records_by_fields("Error getting records by fields",
+       $this->proj->get_records_by_fields("Error getting records by fields",
 				       $fields_filter);
 				       
 
--- extras/test/Regression2Test.php
+++ extras/test/Regression2Test.php
@@ -1,38 +1,21 @@
 <?php
 
-require_once('PHPUnit/Autoload.php');
+require_once('extras/test/PhpCap_TestCase.php');
 
-require_once('SampleNotifier.php');
-require_once('PhpCap.php');
+class Regression2Test extends PhpCap_TestCase {
 
-class Regression2Test extends PHPUnit_Framework_TestCase {
+   public function testRegression2 () {
 
-   public function testRegression1 () {
-
-     $test_name = 'Regression Test 2';
-
-     $from_email = 'aarenson@iu.edu';
-     $subject_token = '['.$test_name.'] Error';
-     $redcap_url = 'https://redcap.uits.iu.edu/api/';
-     $api_token = 'C5477C91801C64BB1C0BD65DEDB6C354';
-     $primary_key = 'record_id';
-
-     // Create factory
-     $app = $test_name;
-     $notifier = new SampleNotifier($from_email,$from_email,$subject_token);
-     $apifactory = new REDCapFactory($app,$redcap_url,$notifier);
-     $project = $apifactory->make_project($api_token,$primary_key);
-      
      // Get results
      $error = "Error in prep_records_by_ids_batch";
      $batch_size = 2;
      $num_records_to_retrieve = 10;
      $record_ids = range(1,$num_records_to_retrieve);
-     $project->prep_records_by_ids_batch($error,$batch_size,$record_ids);
+     $this->proj->prep_records_by_ids_batch($error,$batch_size,$record_ids);
 
      $record_events_retrieved = 0;
 
-     while($batch = $project->get_records_by_ids_batch()) {
+     while($batch = $this->proj->get_records_by_ids_batch()) {
        foreach($batch as $id => $record_events) {
 	 $record_events_retrieved += count($record_events);
        }
