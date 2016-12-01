0d932d24a887be5246c919ea093f5e81bc358131
Andy Arenson <aarenson@iu.edu>
2016-12-01 15:40:56 -0500

    Merge RestCallRequest into PhpCap

--- PhpCap.php
+++ PhpCap.php
@@ -19,10 +19,9 @@
 //--------------------------------------------------------------------------
 // Required libraries
 //--------------------------------------------------------------------------
-require_once('RestCallRequest.php');
+  //require_once('RestCallRequest.php');
 
 //-------------------------------------------------------------------------
-// $Id$
 //
 // REDCapProject is used for importing/exporting via REDCap API
 //
@@ -1009,18 +1008,71 @@ class REDCapDETHandler {
 
       $error = "Project id supplied by data entry trigger ('".$det_id."') ".
 	"does not match expected id for survey ('".$this->pid."').";
-	  $this->notifier->notify($error);
+      $this->notifier->notify($error);
     }
 
-  return true;
-}
+    return true;
+  }
 
-// END check_det_id
-//-------------------------------------------------------------------------
+  // END check_det_id
+  //-------------------------------------------------------------------------
 
 
 }
 // END class REDCapDETHandler
 //-------------------------------------------------------------------------
 
+//-------------------------------------------------------------------------
+//
+// RestCallRequest holds arguments and results for an API call
+//
+//-------------------------------------------------------------------------
+//
+class RestCallRequest {
+
+	protected $url;
+	protected $requestBody;
+	protected $responseBody;
+	protected $responseInfo;
+	
+	public function getResponseBody () {
+		return $this->responseBody;
+	} 
+	public function getResponseInfo () {
+		return $this->responseInfo;
+	} 
+	
+	public function __construct ($url, $verb, $requestBody)	{
+		$this->url	     = $url;
+		$this->requestBody   = http_build_query($requestBody, '', '&');
+		$this->responseBody  = null;
+		$this->responseInfo  = null;
+	}
+	
+	public function execute () {
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+#		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, TRUE);
+
+		// 20 minutes till it times out
+		curl_setopt($ch, CURLOPT_TIMEOUT, 1200); 
+		curl_setopt($ch, CURLOPT_URL, $this->url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, 
+			    array('Accept: text/xml'));
+
+		// Because we are doing a POST
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $this->requestBody);
+		curl_setopt($ch, CURLOPT_POST, 1);
+	
+		$this->responseBody = curl_exec($ch);
+		$this->responseInfo = curl_getinfo($ch);
+		
+		curl_close($ch);
+			
+	}
+	
+}
+// END class RestCallRequest
+//-------------------------------------------------------------------------
 
