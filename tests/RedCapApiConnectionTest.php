6f504de4bdc938650976ebe5444ecef269499b1c
Jim Mullen <mullen2@iu.edu>
2017-04-27 14:27:40 -0400

    Added tests.

--- tests/RedCapApiConnectionTest.php
+++ tests/RedCapApiConnectionTest.php
@@ -1,16 +1,52 @@
 <?php
 
+namespace IU\PHPCap;
+
 use PHPUnit\Framework\TestCase;
 use IU\PHPCap\RedCapApiConnection;
+
+
+function curl_errno($curlHandle) {
+    if (RedCapApiConnectionTest::$curlErrorNumber != 0) {
+        $errno = RedCapApiConnectionTest::$curlErrorNumber;
+    }
+    else {
+        $errno = \curl_errno($curlHandle);
+    }
+    return $errno;
+}
+
+function curl_error($curlHandle) {
+    if (RedCapApiConnectionTest::$curlErrorNumber !== '') {
+        $error = RedCapApiConnectionTest::$curlErrorMessage;
+    }
+    else {
+        $errno = \curl_error($curlHandle);
+    }
+    return $error;
+}
+
 
 class RedCapApiConnectionTest extends TestCase {
     private static $config;
     private static $apiConnection;
     
+    public static $curlErrorNumber;
+    public static $curlErrorMessage;
+    
     public static function setUpBeforeClass()
     {
         self::$config = parse_ini_file('config.ini');
         self::$apiConnection = new RedCapApiConnection(self::$config['api.url']);
+        
+        self::$curlErrorNumber  = 0;
+        self::$curlErrorMessage = '';
+    }
+    
+    public function testConnectionCreation()
+    {
+        $apiConnection = new RedCapApiConnection(self::$config['api.url']);
+        $this->assertNotNull($apiConnection, 'Connection is not null.');
     }
     
     public function testProjectInfo()
@@ -31,7 +67,26 @@ class RedCapApiConnectionTest extends TestCase {
         
         $this->assertEquals($result['project_language'], 'English', 'Project info "project_language" test.');
         $this->assertEquals($result['purpose_other'], 'PHPCap testing', 'Project info "purpose_other" test.');
-
+        
+        $callInfo = self::$apiConnection->getCallInfo();
+        $this->assertTrue(array_key_exists('url', $callInfo), "callInfo has 'url' key.");
+        $this->assertEquals($callInfo['url'], self::$config['api.url'], 'callInfo URL is correct.');
+        
+        self::$curlErrorNumber = 3;
+        self::$curlErrorMessage = 'The URL was not properly formatted.';
+        $exceptionCaught = false;
+        try {
+            $callInfo = self::$apiConnection->getCallInfo();
+        }
+        catch (PHPCapException $exception) {
+            $exceptionCaught = true;
+            $this->assertEquals($exception->getCode(), PhpCapException::CURL_ERROR);
+            $this->assertEquals($exception->getCurlErrorNumber(), self::$curlErrorNumber);
+            $this->assertEquals($exception->getMessage(), self::$curlErrorMessage);
+        }
+        $this->assertTrue($exceptionCaught);
+        self::$curlErrorNumber = 0;
+        self::$curlErrorMessage = '';
     }
     
     public function testTimeout()
