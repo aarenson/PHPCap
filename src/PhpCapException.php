70590a9b0e90724772c2a62cbc102b5aa65adec5
Jim Mullen <mullen2@iu.edu>
2017-05-16 16:00:45 -0400

    Reordered PhpCapException error codes and updated the API doc.

--- src/PhpCapException.php
+++ src/PhpCapException.php
@@ -38,39 +38,39 @@ class PhpCapException extends \Exception
     /** Invalid argument passed to a PHPCap method. */
     const INVALID_ARGUMENT = 1;
     
-    /** A cURL error occurred. */
-    const CURL_ERROR = 2;
+    /** Too many arguments were passed to the method. */
+    const TOO_MANY_ARGUMENTS = 2;
+
+    /** An invalid URL was used. */
+    const INVALID_URL = 3;
     
     /** A CA certificate file was specified, but it could not be found. */
-    const CA_CERTIFICATE_FILE_NOT_FOUND = 3;
+    const CA_CERTIFICATE_FILE_NOT_FOUND = 4;
     
     /** The CA certificate file could not be read. */
-    const CA_CERTIFICATE_FILE_UNREADABLE = 4;
-    
-    /** An invalid URL was used. */
-    const INVALID_URL = 5;
+    const CA_CERTIFICATE_FILE_UNREADABLE = 5;
+
+    /** A cURL error occurred. */
+    const CURL_ERROR = 6;
     
     /** The REDCap API generated an error. */
-    const REDCAP_API_ERROR = 6;   // An error response from the REDCap API
+    const REDCAP_API_ERROR = 7;
     
     /** A JSON error occurred. This would typically happen when PHPCap is expecting
      * the REDCap API to return data in JSON format, but the result returned is not valid JSON.
      */
-    const JSON_ERROR = 7;
+    const JSON_ERROR = 8;
     
     /** The output file could not be found, or was found and could not be written */
-    const OUTPUT_FILE_ERROR     = 8;
-    
-    /** Too many arguments were passed to the method. */
-    const TOO_MANY_ARGUMENTS    = 9;
-    
+    const OUTPUT_FILE_ERROR     = 9;
+
     /** The input file could not be found. */
     const INPUT_FILE_NOT_FOUND  = 10;
     
-    /** The input file was found, but is unreadbale */
+    /** The input file was found, but is unreadable. */
     const INPUT_FILE_UNREADABLE = 11;
     
-    /** The input file contents are invalid */
+    /** The input file contents are invalid. */
     const INPUT_FILE_ERROR      = 12;
     
 
@@ -88,7 +88,7 @@ class PhpCapException extends \Exception
      * @param integer $code the error code.
      * @param integer $curlErrorNumber the cURL error number (set to null if no cURL error occurred).
      * @param integer $httpStatusCode the HTTP status code (set to null if no HTTP status code was returned).
-     * @param \Exception $previous the previous exception
+     * @param \Exception $previous the previous exception.
      */
     public function __construct($message, $code, $curlErrorNumber = null, $httpStatusCode = null, $previous = null)
     {
