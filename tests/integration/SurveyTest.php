48cfa2bbe52c66a342ed5cb2255b43421fad87f5
Jim Mullen <mullen2@iu.edu>
2017-06-13 16:33:51 -0400

    Improved automated tests for survey methods.

--- tests/integration/SurveyTest.php
+++ tests/integration/SurveyTest.php
@@ -13,6 +13,7 @@ class SurveyTest extends TestCase
 {
     private static $config;
     private static $apiUrl;
+    private static $apiToken;
     private static $repeatableSurveyProject;
     private static $participantEmail;
     private static $participantIdentifier;
@@ -23,14 +24,14 @@ class SurveyTest extends TestCase
         
         self::$apiUrl = self::$config['api.url'];
         
-        $apiToken = self::$config['repeatable.survey.api.token'];
+        self::$apiToken = self::$config['repeatable.survey.api.token'];
         
         self::$participantEmail      = self::$config['survey.participant.email'];
         self::$participantIdentifier = self::$config['survey.participant.identifier'];
         
         self::$repeatableSurveyProject = null;
-        if ($apiToken != null && $apiToken !== '') {
-            self::$repeatableSurveyProject = new RedCapProject(self::$apiUrl, $apiToken);
+        if (self::$apiToken != null && self::$apiToken !== '') {
+            self::$repeatableSurveyProject = new RedCapProject(self::$apiUrl, self::$apiToken);
         }
     }
     
@@ -41,7 +42,9 @@ class SurveyTest extends TestCase
             $form = "weight";
             $surveyLink = self::$repeatableSurveyProject->exportSurveyLink($recordId, $form);
             
-            $this->assertNotNull($surveyLink, 'Non-null survey link check');
+            $this->assertNotNull($surveyLink, 'Non-null survey link check.');
+            
+            $this->assertStringStartsWith('http', $surveyLink, 'Survey link starts with "http".');
         }
     }
     
@@ -58,6 +61,58 @@ class SurveyTest extends TestCase
                 $this->assertEquals(PhpCapException::INVALID_ARGUMENT, $code, 'Exception code check.');
             }
             $this->assertTrue($caughtException, 'Exception caught check.');
+        }
+    }
+    
+    public function testExportSurveyParticipants()
+    {
+        if (self::$repeatableSurveyProject != null) {
+            $form = 'weight';
+            $surveyParticipants = self::$repeatableSurveyProject->exportSurveyParticipants($form);
+
+            $emailFound = false;
+            $identifierFound = false;
+            foreach ($surveyParticipants as $participant) {
+                if ($participant['email'] === self::$participantEmail) {
+                    $emailFound = true;
+                }
+                if ($participant['identifier'] === self::$participantIdentifier) {
+                    $identifierFound = true;
+                }
+                if ($emailFound === true && $identifierFound === true) {
+                    break;
+                }
+            }
+            
+            $this->assertTrue($emailFound, 'Participant e-mail found.');
+            $this->assertTrue($identifierFound, 'Participant identifier found.');
+        }
+    }
+    
+    
+    public function testExportSurveyQueueLink()
+    {
+        if (self::$repeatableSurveyProject != null) {
+            $recordId = 1;
+            $form = "weight";
+            $surveyQueueLink = self::$repeatableSurveyProject->exportSurveyQueueLink($recordId, $form);
+            
+            $this->assertNotNull($surveyQueueLink, 'Non-null survey queue link check.');
+            
+            $this->assertStringStartsWith('http', $surveyQueueLink, 'Survey queue link starts with "http".');
+        }
+    }
+    
+    
+    public function testExportSurveyReturnCode()
+    {
+        if (self::$repeatableSurveyProject != null) {
+            $recordId = 1;
+            $form = "weight";
+            $surveyReturnCode = self::$repeatableSurveyProject->exportSurveyReturnCode($recordId, $form);
+            
+            $this->assertNotNull($surveyReturnCode, 'Non-null survey return code check.');
+            $this->assertTrue(ctype_alnum($surveyReturnCode), 'Alphanumeric survey return code check.');
         }
     }
 }
